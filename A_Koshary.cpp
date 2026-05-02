618bbb33d303f9911f8bd4e591acfcaff4e8430f
Aaradhya Mehra <aaradhyamehra240@gmail.com>
2026-05-02 10:28:25 +0530

    2/5/26

---
+++ A_Koshary.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int t;
+    cin>>t;
+    while(t--){
+        int x,y;
+        cin>>x>>y;
+        if(x%2==0 &y%2==0) cout<<"YES"<<endl;
+        else if(x%2==1 &y%2==1) cout<<"NO"<<endl;
+        else if((x%2==1&y%2==0) or (x%2==0&y%2==1)) cout<<"YES"<<endl;
+
+    }
+}
